33e24ee053a95c0916e8391db5c0ec3dae55bdc1
SergiuNani <130356254+SergiuNani@users.noreply.github.com>
2024-05-27 20:13:14 +0200

    052701

--- C# Code 1/Program.cs
+++ C# Code 1/Program.cs
@@ -1,13 +1,4 @@
-﻿/*
-Import namespace called system that has many classes
-and useful functions (Without it we'd have to type
-System.Console.Write)
-*/
-using System;
-using System.Globalization;
-using System.Text;
-
-
+﻿
 namespace HelloWorld {
     // Our class name is Program
     public class Program
